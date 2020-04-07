833e76120abfdd0ee5835b6e6b7d1867d4a7c99d
osamahabib5 <osama.habib@khi.iba.edu.pk>
2020-04-07 22:37:28 +0500

    mapDispatchtoProps

--- redux_app/src/components/ItemContainer.js
+++ redux_app/src/components/ItemContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyIceCream, buyCake } from '../redux'
 function ItemContainer(props) {
     return (
         <div>
             <h2>Item -{props.item} </h2>
+            <button onClick={props.buyItem} >Buy Items</button>
         </div>
     )
 }
@@ -14,4 +16,12 @@ const mapStatetoProps = (state, ownProps) => {  //own props define props that ow
     }
 }
 
-export default connect(mapStatetoProps)(ItemContainer)
+const dispatchtoProps = (dispatch, ownProps) => {
+    const dispatchFunction = ownProps.cake ?
+        () => dispatch(buyCake()) : () => dispatch(buyIceCream())
+    return {
+        buyItem: dispatchFunction
+    }
+}
+
+export default connect(mapStatetoProps, dispatchtoProps)(ItemContainer)
